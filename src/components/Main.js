be09206dfabddabce839fcea435737827bec3441
Alex Villecco <amvillecco@teco.com.ar>
2022-03-02 20:02:50 -0300

    organize space in Header

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,21 @@
-import { Layout, Input } from 'antd';
+import { Layout, Input, Row, Col, } from 'antd';
 const { Header, Footer, Sider, Content } = Layout;
 const { Search } = Input;
 
 const Main = ({ userName }) => {
   return(
     <Layout>
-      <Header>
-        Encabezado
+      <Header className="header">
+        <Row>
+          <Col xs={{ span: 5}} lg={{ span: 3 }}>Logo</Col>
+          <Col xs={{ span: 19}} lg={{ span: 16 }}>Buscador</Col>
+          <Col xs={{ span: 0}} lg={{ span: 5 }}>Bienvenido {userName}</Col>
+        </Row>
       </Header>
-      <Content>
-        <p style={{color: 'black'}}>{userName}</p>
+      <Content className='content'>
+        <p style={{color: 'black'}}>Contenido</p>
       </Content>
-      <Footer>
+      <Footer className='footer'>
         Pie
       </Footer>
     </Layout>
