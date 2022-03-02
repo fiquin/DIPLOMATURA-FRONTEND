7dea4a7f52d347e4ba0d7433988f4adaa09dfa36
Alex Villecco <amvillecco@teco.com.ar>
2022-03-02 20:07:55 -0300

    add Search bar in Header

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,7 +8,15 @@ const Main = ({ userName }) => {
       <Header className="header">
         <Row>
           <Col xs={{ span: 5}} lg={{ span: 3 }}>Logo</Col>
-          <Col xs={{ span: 19}} lg={{ span: 16 }}>Buscador</Col>
+          <Col xs={{ span: 19}} lg={{ span: 16 }}>
+            <div className="header-search">
+              <Search
+                placeholder='¿Que estás buscando?'
+                onSearch={value => console.log(value)}
+                enterButton
+              />
+            </div>
+          </Col>
           <Col xs={{ span: 0}} lg={{ span: 5 }}>Bienvenido {userName}</Col>
         </Row>
       </Header>
