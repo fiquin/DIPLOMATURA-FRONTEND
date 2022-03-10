a5605d8a25b7577c9bfe32f7133bb3e0a391475a
Alex Villecco <amvillecco@teco.com.ar>
2022-03-09 21:19:20 -0300

    update new product values when type in fields

--- src/components/NewProduct.js
+++ src/components/NewProduct.js
@@ -1,32 +1,56 @@
-import { Layout, Row, Col, } from 'antd';
+import { Layout, Row, } from 'antd';
+import React, { useState } from 'react';
 const { Content } = Layout;
 
-const NewProduct = ({ setScreen }) => (
-  <>
-    <Content className='content'>
-      <button className='button' onClick={() => setScreen(0)}>Volver</button>
-      <p>Ingresá tu producto</p>
-      <Row className="products-container">
-        <form >
-          <div className='form'>
-            <div className="form-field">
-              <label className="label" htmlFor="name">Nombre</label>
-              <input type="text" />
+const NewProduct = ({ setScreen }) => {
+  const [ values, setValues ] = useState({
+    productName: '',
+    description: '',
+    price: 0,
+    imageUrl: ''
+  });
+  
+  const handleChange = (event) => {
+    event.preventDefault();
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setValues(prev => ({...prev, [name]: value}))
+  }
+
+  console.log("🚀 :::::::::::: values", values);
+
+  return(
+    <>
+      <Content className='content'>
+        <button className='button' onClick={() => setScreen(0)}>Volver</button>
+        <p>Ingresá tu producto</p>
+        <Row className="products-container">
+          <form >
+            <div className='form'>
+              <div className="form-field">
+                <label className="label" htmlFor="productName">Nombre</label>
+                <input onChange={handleChange} value={values.productName} name="productName" style={{ color: 'black'}} type="text" />
+              </div>
+              <div className="form-field">
+                <label className="label" htmlFor="description">Descripción</label>
+                <input onChange={handleChange} value={values.description} name="description" style={{ color: 'black'}} type="text" />
+              </div>
+              <div className="form-field">
+                <label className="label" htmlFor="price">Precio</label>
+                <input onChange={handleChange} value={values.price} name="price" style={{ color: 'black'}} type="number" />
+              </div>
+              <div className="form-field">
+                <label className="label" htmlFor="imageUrl">Imagen</label>
+                <input onChange={handleChange} value={values.imageUrl} name="imageUrl" style={{ color: 'black'}} type="text" />
+              </div>
+              <input style={{ color: 'black' }} type="submit" value="Enviar" />
             </div>
-            <div className="form-field">
-              <label className="label" htmlFor="price">Precio</label>
-              <input type="number" />
-            </div>
-            <div className="form-field">
-              <label className="label" htmlFor="imageUrl">Imagen</label>
-              <input type="text" />
-            </div>
-            <input style={{ color: 'black' }} type="submit" value="Enviar" />
-          </div>
-        </form>
-      </Row>
-    </Content>
-  </>
-);
+          </form>
+        </Row>
+      </Content>
+    </>
+  );
+}
 
 export default NewProduct;
